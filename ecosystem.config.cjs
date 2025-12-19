50a43b8a20eb6c7dc15c5b9fee72faa2348256cb
Developer <developer@example.com>
2025-12-20 02:23:14 +0700

    Use npx tsx for PM2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,8 +4,8 @@ module.exports = {
     {
       name: 'mini-ielts-score',
       script: 'server/index.ts',
-      interpreter: 'node',
-      interpreter_args: '-r tsx',
+      interpreter: 'npx',
+      interpreter_args: 'tsx',
       instances: 'max', // Tự động dùng tất cả CPU cores (hoặc số cụ thể như 2, 4)
       exec_mode: 'cluster', // Cluster mode để dùng nhiều CPU cores
       env: {
